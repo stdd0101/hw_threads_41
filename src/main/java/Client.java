675733d81e498813c45009c4b81bcbdbc4d47acf
stdd01 <stdd01@mail.ru>
2021-04-25 14:06:07 +0300

    fix

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,33 +2,23 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-
-    private static Socket clientSocket;
-    private static BufferedReader reader;
-    private static BufferedReader in;
-    private static BufferedWriter out;
-
     public static void main(String[] args) {
-        try {
-            try {
-                clientSocket = new Socket("localhost", 40045);
-                reader = new BufferedReader(new InputStreamReader(System.in));
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        try (
+                Socket clientSocket = new Socket("localhost", 40045);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        ) {
 
-                System.out.println("Введите n:");
-                String word = reader.readLine();
-                out.write(word + "\n");
-                out.flush();
-                String serverWord = in.readLine();
-                System.out.println(serverWord);
-            } finally {
-                System.out.println("Завершение работы клиента.");
-                clientSocket.close();
-                in.close();
-                out.close();
-            }
+            System.out.println("Введите n:");
+            String word = reader.readLine();
+            out.write(word + "\n");
+            out.flush();
+            String serverWord = in.readLine();
+            System.out.println(serverWord);
+
         } catch (IOException e) {
+            System.out.println("Завершение работы клиента.");
             System.err.println(e);
         }
     }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,45 +3,30 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-
-    private static Socket clientSocket;
-    private static ServerSocket server;
-    private static BufferedReader in;
-    private static BufferedWriter out;
-
     public static int fibonacci(int n) {
-        if(n == 1 || n == 2) {
+        if (n == 1 || n == 2) {
             return 1;
         } else {
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
+
     public static void main(String[] args) {
-        try {
-            try {
-                server = new ServerSocket(40045);
-                System.out.println("Сервер запущен!");
-                clientSocket = server.accept();
-                try {
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        try (
+                ServerSocket server = new ServerSocket(40045);
+                Socket clientSocket = server.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))
+        ) {
+            System.out.println("Сервер запущен!");
 
-                    String input = in.readLine();
-                    System.out.println(input);
-                    int number = Integer.parseInt(input);
-                    out.write("Привет, это Сервер! Число Фибоначчи: " + fibonacci(number) + "\n");
-                    out.flush();
-
-                } finally {
-                    clientSocket.close();
-                    in.close();
-                    out.close();
-                }
-            } finally {
-                System.out.println("Сервер закрыт!");
-                server.close();
-            }
+            String input = in.readLine();
+            System.out.println(input);
+            int number = Integer.parseInt(input);
+            out.write("Привет, это Сервер! Число Фибоначчи: " + fibonacci(number) + "\n");
+            out.flush();
         } catch (IOException e) {
+            System.out.println("Сервер закрыт!");
             System.err.println(e);
         }
     }
